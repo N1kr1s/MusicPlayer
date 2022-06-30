7a34ecaca0646d3b8ba3422268f8625aa535dbff
Nikris <nikris@mail.ru>
2022-06-30 23:58:36 +0300

    halfway there

---
+++ music.js
@@ -0,0 +1,26 @@
+export const allMusic = [
+  {
+    artist: 'FLESH',
+    songName: 'Creep The Line',
+    src: './music/CreepTheLine.mp3',
+    img: './img/one.jpg',
+  },
+  {
+    artist: 'Fraunhofer Diffraction',
+    songName: 'Somewhere But Not Here',
+    src: './music/SomewhereButNotHere.mp3',
+    img: './img/two.jpg',
+  },
+  {
+    artist: 'mark veins & STAHL',
+    songName: 'Clear',
+    src: './music/Clear.mp3',
+    img: './img/three.jpg',
+  },
+  {
+    artist: 'Michael FK & Faodail',
+    songName: 'Holding Back',
+    src: './music/HoldingBack.mp3',
+    img: './img/four.jpg',
+  },
+];
--- script.js
+++ script.js
@@ -1,22 +1,70 @@
+import { allMusic } from './music.js';
+
 class MusicPlayer {
   constructor() {
-    this.music = document.querySelector('audio');
+    this.num = 3;
+    this.data = allMusic[this.num];
+    this.songAndArtist = document.getElementById('songAndArtist');
+    this.playerContainer = document.getElementById('player-container');
+    this.artistAndMusic();
     this.prevBtn = document.getElementById('prev');
-    this.playBtn = document.getElementById('play');
-    this.pauseBtn = document.getElementById('pause');
+    this.playPauseBtn = document.getElementById('play/pause');
     this.nextBtn = document.getElementById('next');
-    this.playBtn.addEventListener('click', this.playSong.bind(this));
-    this.pauseBtn.addEventListener('click', this.pauseSong.bind(this));
+    this.isPlaying = false;
+    this.music = document.querySelector('audio');
+    this.currentTime = document.getElementById('current-time');
+    this.duration = document.getElementById('duration');
+
+    this.playPauseBtn.addEventListener('click', () => {
+      this.isPlaying ? this.pauseSong() : this.playSong();
+    });
+    this.prevBtn.addEventListener('click', () => {
+      if (this.num <= 0) this.num = allMusic.length - 1;
+      else this.num--;
+      //*Dont forget to update data!!
+      this.data = allMusic[this.num];
+      this.pauseSong();
+      this.artistAndMusic();
+    });
+    this.nextBtn.addEventListener('click', () => {
+      if (this.num === allMusic.length - 1) this.num = 0;
+      else this.num++;
+      this.data = allMusic[this.num];
+      this.pauseSong();
+      this.artistAndMusic();
+    });
+
+    this.music.addEventListener('timeupdate', (e) => {
+      this.currentTime.innerText = e.target.currentTime;
+      this.duration.innerText = e.target.duration;
+    });
   }
+
+  artistAndMusic() {
+    const { data, songAndArtist } = this;
+    const artistAndMusic = document.createElement('div');
+    artistAndMusic.innerHTML = `
+    <div class="img-container">
+    <img src=${data.img} alt=${data.songName}/>
+    </div>
+    <h2 id="title">${data.songName}</h2>
+    <h3 class="artist" id="artist">${data.artist}</h3>
+    <audio src=${data.src} ></audio>
+    `;
+    if (songAndArtist.hasChildNodes()) songAndArtist.innerHTML = '';
+    songAndArtist.appendChild(artistAndMusic);
+    this.music = document.querySelector('audio');
+  }
+
   playSong() {
     this.music.play();
-    this.playBtn.classList.add('hide');
-    this.pauseBtn.classList.remove('hide');
+    this.isPlaying = true;
+    this.playPauseBtn.classList.replace('fa-play', 'fa-pause');
   }
   pauseSong() {
     this.music.pause();
-    this.playBtn.classList.remove('hide');
-    this.pauseBtn.classList.add('hide');
+    this.isPlaying = false;
+    this.playPauseBtn.classList.replace('fa-pause', 'fa-play');
   }
 }
 
